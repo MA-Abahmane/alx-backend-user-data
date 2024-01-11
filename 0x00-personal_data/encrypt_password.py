f03e65ab01c2d05dd96355dc24567575d7e4af60
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-11 14:58:48 +0100

    Push Task 5 & 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 """
-
+Encrypting passwords
+User passwords should NEVER be stored in plain text in a database.
 """
 
+import bcrypt
+from typing import ByteString
+
+
+def hash_password(password: str) -> ByteString:
+    """ takes in password and returns a hashed password """
+    # Hash a password for the first time, with a randomly-generated salt
+    hashed_pwd = bcrypt.hashpw((password.encode('UTF-8')),
+                               bcrypt.gensalt())
+    return hashed_pwd
+
+
+def is_valid(hashed_password: ByteString, password: str) -> bool:
+    """ validate that the provided password matches the hashed password """
+    # Check that an unhashed password matches one that has previously been
+    # hashed
+    return bcrypt.checkpw(password.encode('UTF-8'), hashed_password)
