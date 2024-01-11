277b66a0523de5dbcddbede76c72a8f3b2f45162
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-11 14:13:59 +0100

    Update Task 1 VI

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,13 +20,13 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """ Constructor """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.flds = fields
