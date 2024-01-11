164cf56411d25d219ac5ac16a9186b2095120c3a
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-11 13:40:52 +0100

    Update Task 0 III

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,11 @@
 A function named filter_datum that returns the log message obfuscated
 """
 
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ A function that returns the log message obfuscated """
     for field in fields:
         fltrd_message = re.sub(f'{field}=.*?{separator}',
