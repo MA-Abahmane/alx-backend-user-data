e657af417f7260fc5a3b60d82c886783757a586e
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-11 14:03:38 +0100

    Update Task 1 II

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,13 +4,36 @@
 A function named filter_datum that returns the log message obfuscated
 """
 
-from typing import List
 import re
+import logging
+from tkinter import SEPARATOR
+from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ A function that returns the log message obfuscated """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
-                                f'{field}={redaction}{separator}', message)
+                         f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.flds = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum """
+        record.msg = filter_datum(self.flds, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+
+        return super(RedactingFormatter, self).format(record)
