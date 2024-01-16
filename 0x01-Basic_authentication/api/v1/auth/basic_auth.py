8c5b884ced33bcb1fa503b43701e4faa4c7b29f9
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-16 23:40:56 +0100

    Update Task files

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,8 @@
 """
 
 import base64
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -34,3 +36,36 @@ class BasicAuth(Auth):
             return value.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None or\
+            type(decoded_base64_authorization_header) != str or\
+                ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        values = decoded_base64_authorization_header.split(':')
+        return values[0], values[1]
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """  returns the User instance based on his email and password
+        """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        if len(users) <= 0:
+            return None
+
+        if users[0].is_valid_password(user_pwd):
+            return users[0]
+        return None
