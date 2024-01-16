aee179f03fd250f200bbac2f88ee4ad73bc7488e
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-16 12:41:01 +0100

    Pycodestyle fix

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,7 +27,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False) # type: ignore
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
@@ -36,7 +36,7 @@ def unauthorized() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False) # type: ignore
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> None:
     """ GET /api/v1/forbidden
     Return:
