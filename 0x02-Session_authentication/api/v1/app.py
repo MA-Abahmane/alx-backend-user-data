7a08ee9add485ec6efc36fb5b92a94e240935514
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-17 16:16:42 +0100

    Pycodestyle checked II

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,8 +36,8 @@ def authentication():
     if auth is None:
         return
     if auth.require_auth(request.path, paths):
-        if auth.authorization_header(request) is None and\
-            auth.session_cookie(request) is None:
+        if auth.authorization_header(request) is None \
+                and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
