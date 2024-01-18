ffdab70384e28018c677e0be2657461f3bf97f71
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-18 19:19:53 +0100

    add User Session Model

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Sessions in database
+DocDocDocDocDoc
+"""
+
+from models.base import Base
+
+
+class UserSession(Base):
+    """ UserSession class """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Constructor
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
