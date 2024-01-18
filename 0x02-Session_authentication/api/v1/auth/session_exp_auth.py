9fa0c7f9e6b90ae0733007f283dcaf0c1e16bce5
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-18 19:33:22 +0100

    bug fix II

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -17,7 +17,7 @@ class SessionExpAuth(SessionAuth):
     def __init__(self):
         """ Constructor
         """
-        self.session_duration = getenv('SESSION_DURATION', 0)
+        self.session_duration = int(getenv('SESSION_DURATION', 0))
 
     def create_session(self, user_id=None):
         """ session creation
