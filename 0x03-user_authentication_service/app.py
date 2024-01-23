b0e88deb1d8f9373fff59aeb3e42cf9f7e103147
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-23 17:52:55 +0100

    Update APP test

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -108,7 +108,7 @@ def update_password():
     except ValueError:
         abort(403)
 
-    return jsonify({"email": email, "message": "Password updated"}), 200
+    return jsonify({"email": email, "message": "Password updated"})
 
 
 if __name__ == "__main__":
