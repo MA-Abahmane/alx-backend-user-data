e0e8868a426fabac49e2478166c13d77450611c7
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-23 17:45:33 +0100

    Update APP VI

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,7 +79,7 @@ def profile():
     abort(403)
 
 
-@app.route('/reset_password ', methods=['POST'], strict_slashes=False)
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     """  reset User password token
     """
@@ -93,7 +93,7 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": new_token}), 200
 
 
-@app.route('/reset_password  ', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """  reset User password token
     """
