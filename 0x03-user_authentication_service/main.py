65ffc0ac7bd0146b7d75e7e05af2fc17a4e80241
Mohamed Amine <ma.abahmane@gmail.com>
2024-01-23 18:01:01 +0100

    Push main

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+"""
+The Main Module
+"""
+
+import requests
+
+from app import AUTH
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+def register_user(email: str, password: str) -> None:
+    """ DocDoc
+    """
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ DocDoc
+    """
+
+def log_in(email: str, password: str) -> str:
+    """ DocDoc
+    """
+
+def profile_unlogged() -> None:
+    """ DocDoc
+    """
+
+def profile_logged(session_id: str) -> None:
+    """ DocDoc
+    """
+
+def log_out(session_id: str) -> None:
+    """ DocDoc
+    """
+
+def reset_password_token(email: str) -> str:
+    """ DocDoc
+    """
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """ DocDoc
+    """
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
